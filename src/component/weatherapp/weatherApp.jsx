2405096955284366f14c92c9339068b26f9ff68c
Amman Gillani <ammangillani4321@icloud.com>
2024-02-26 17:13:00 +0500

    cmf

--- src/component/weatherapp/weatherApp.jsx
+++ src/component/weatherapp/weatherApp.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './weatherApp.css';
-
+import clearicon from "../assets/clear.png";
+import searchicon from "../assets/search.png";
+import rainicon from "../assets/rain.png";
+import snow from "../assets/snow.png";
+import cloudicon from "../assets/cloud.png";
+import drizzleicon from "../assets/drizzle.png";
+import humidityicon from "../assets/humidity.png";
  const weatherApp = () => {
   return (
-    <div>am</div>
+    <div className='container'>
+      <div className="top-bar">
+            <input type="text" className='cityInput' placeholder='Search'/>
+            <div className="search-icon">
+              <img src={searchicon} alt="" />
+            </div>
+      </div>
+    </div>
   )
 }
 
