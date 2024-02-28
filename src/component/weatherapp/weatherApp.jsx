f5a14f3373ddbe6e31622948ba0eb8bbd60535d0
Amman Gillani <ammangillani4321@icloud.com>
2024-02-28 14:42:42 +0500

    commit

--- src/component/weatherapp/weatherApp.jsx
+++ src/component/weatherapp/weatherApp.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './weatherApp.css';
+import React from "react";
+import "./weatherApp.css";
 import clearicon from "../assets/clear.png";
 import searchicon from "../assets/search.png";
 import rainicon from "../assets/rain.png";
@@ -7,17 +7,31 @@ import snow from "../assets/snow.png";
 import cloudicon from "../assets/cloud.png";
 import drizzleicon from "../assets/drizzle.png";
 import humidityicon from "../assets/humidity.png";
- const weatherApp = () => {
+const weatherApp = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <div className="top-bar">
-            <input type="text" className='cityInput' placeholder='Search'/>
-            <div className="search-icon">
-              <img src={searchicon} alt="" />
-            </div>
+        <input type="text" className="cityInput" placeholder="Search" />
+        <div className="search-icon">
+          <img src={searchicon} alt="" />
+        </div>
+      </div>
+      <div className="weather-img">
+        <img src={cloudicon} alt="" />
+      </div>
+      <div className="weather-temp">24 c</div>
+      <div className="weather-loc">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src="" alt="" />
+
+          <div className="data">
+            <div className="humidity-per">64%</div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default weatherApp
+export default weatherApp;
