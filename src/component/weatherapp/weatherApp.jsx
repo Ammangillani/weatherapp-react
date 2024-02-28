8e21cefd23820585742b6cfdebc2746a2045b8fb
Amman Gillani <ammangillani4321@icloud.com>
2024-02-28 15:32:42 +0500

    commiy

--- src/component/weatherapp/weatherApp.jsx
+++ src/component/weatherapp/weatherApp.jsx
@@ -7,6 +7,8 @@ import snow from "../assets/snow.png";
 import cloudicon from "../assets/cloud.png";
 import drizzleicon from "../assets/drizzle.png";
 import humidityicon from "../assets/humidity.png";
+import windicon from "../assets/wind.png";
+
 const weatherApp = () => {
   return (
     <div className="container">
@@ -19,14 +21,23 @@ const weatherApp = () => {
       <div className="weather-img">
         <img src={cloudicon} alt="" />
       </div>
-      <div className="weather-temp">24 c</div>
+      <div className="weather-temp">24&deg;c</div>
       <div className="weather-loc">London</div>
       <div className="data-container">
         <div className="element">
-          <img src="" alt="" />
-
+          <img src={humidityicon} alt="" />
           <div className="data">
             <div className="humidity-per">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+      </div>
+      <div className="data-container">
+        <div className="element">
+          <img src={windicon} alt="" />
+          <div className="data">
+            <div className="humidity-per">18 km/h</div>
+            <div className="text">Wind Speed</div>
           </div>
         </div>
       </div>
